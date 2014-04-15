8445fa0393f17cb05a154ea1ca347eee9b1e184f
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-14 22:30:22 -0400

    Add route comments and a /users route

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'haml'
 
+# GET home page.
 get '/' do
   haml :index, locals: { title: 'Sinatra' }
+end
+
+# GET users listing.
+get '/users' do
+  'respond with a resource'
 end
